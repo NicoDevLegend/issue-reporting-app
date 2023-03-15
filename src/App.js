e413c95d25546262ef9ef52c9bcd7cc98093f7f2
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-15 12:36:31 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Auth0ProviderWithNavigate from "./components/Auth0Provider";
+import { Auth0ProviderWithNavigate } from "./components/Auth0Provider";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -10,19 +10,19 @@ import NavBar from "./components/Navbar";
 function App() {
   return (
     <Auth0ProviderWithNavigate>
-    <div className="App">     
-      <div className="d-flex vh-100">
-        <SideBar />
-        <div className="w-100">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/callback" element={<CallbackPage />} />
-        </Routes>
+      <div className="App">
+        <div className="d-flex vh-100">
+          <SideBar />
+          <div className="w-100">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/callback" element={<CallbackPage />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
     </Auth0ProviderWithNavigate>
   );
 }
