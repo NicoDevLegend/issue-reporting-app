9f43e57491de8f447fdb259b767fd0bcc998efd3
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-15 16:58:38 -0300

    v1

--- src/pages/callback-page.js
+++ src/pages/callback-page.js
@@ -2,9 +2,9 @@ import NavBar from "../components/Navbar";
 
 export const CallbackPage = () => {
   return (
-    <div className="page-layout">
+    <div className="vh-100">
       <NavBar />
-      <div className="page-layout__content" />
+      <div className="vh-75" />
     </div>
   );
 };
