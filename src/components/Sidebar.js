95cca33d3eadc03887f0e706f366da18ee79a01d
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-16 17:55:29 -0300

    v1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,8 +7,11 @@ import {
   CDBSidebarMenuItem,
 } from "cdbreact";
 import { NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function SideBar() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <CDBSidebar textColor="#fff" backgroundColor="#212529">
       <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
@@ -20,33 +23,36 @@ export default function SideBar() {
           Sidebar
         </a>
       </CDBSidebarHeader>
+      {isAuthenticated && (
+        <>
+          <CDBSidebarContent className="sidebar-content">
+            <CDBSidebarMenu>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "activeClicked" : ""
+                }
+              >
+                <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink
+                to="/profile"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "activeClicked" : ""
+                }
+              >
+                <CDBSidebarMenuItem icon="user">
+                  Profile page
+                </CDBSidebarMenuItem>
+              </NavLink>
+            </CDBSidebarMenu>
+          </CDBSidebarContent>
 
-      <CDBSidebarContent className="sidebar-content">
-        <CDBSidebarMenu>
-          <NavLink
-            to="/"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "activeClicked" : ""
-            }
-          >
-            <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink
-            to="/profile"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "activeClicked" : ""
-            }
-          >
-            <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-          </NavLink>
-        </CDBSidebarMenu>
-      </CDBSidebarContent>
-
-      <CDBSidebarFooter className="text-center">
-        <div className="px-20 py-5">
-          Sidebar Footer
-        </div>
-      </CDBSidebarFooter>
+          <CDBSidebarFooter className="text-center">
+            <div className="px-20 py-5">Sidebar Footer</div>
+          </CDBSidebarFooter>
+        </>
+      )}
     </CDBSidebar>
   );
 }
