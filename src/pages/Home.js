7791ccbca35ad9e9951e1332b14efefc9e7e0d5c
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-19 00:26:26 -0300

    v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import "./Home.css";
 
 export default function Home() {
   return (
-  <div className="home">
-    Home
+  <div className="d-flex justify-content-center align-items-center">
+    <h2>This is a <strong>Demo Web Application</strong> named <strong>"Issue Reporter"</strong></h2>
     </div>
   );
 }
