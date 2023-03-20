d4474c00df988f5047437864e77d152ef37d8ec9
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-20 14:16:25 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import SideBar from "./components/Sidebar";
-import NavBar from "./components/Navbar";
+//import SideBar from "./components/Sidebar";
+//import NavBar from "./components/Navbar";
 import Loading from "./components/Loading";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/NotFound";
+//import { Routes, Route } from "react-router-dom";
+//import Home from "./pages/Home";
+//import Profile from "./pages/Profile";
+//import NotFound from "./pages/NotFound";
 
 function App() {
   const { isLoading } = useAuth0();
 
-  return isLoading ? (
-    <Loading />
-  ) : (
+  return isLoading && <Loading />; /*  : (
     <div className="App">
       <div className="d-flex vh-100">
         <SideBar />
@@ -27,7 +25,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  );*/
 }
 
 export default App;
