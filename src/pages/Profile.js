d26b4fa2a5fe1278c1311733f363ad15de98f59e
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-20 15:40:41 -0300

    v1

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,15 +4,16 @@ import Loading from "../components/Loading";
 export default function Profile() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  return isLoading ? (
-    <Loading />
-  ) : (
-    isAuthenticated && (
+  return (
+    isAuthenticated &&
+    (isLoading ? (
+      <Loading />
+    ) : (
       <div className="d-flex flex-column justify-content-center align-items-center h-75 gap-3">
         <img src={user.picture} alt={user.name} />
         <h2>{user.username}</h2>
         <p>{user.email}</p>
       </div>
-    )
+    ))
   );
 }
