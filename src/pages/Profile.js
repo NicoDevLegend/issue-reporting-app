1b20bb00dba9af566ef4cef1ad31704a5704ed75
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-23 11:28:31 -0300

    v1

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "../components/Loading";
 
@@ -11,10 +13,25 @@ export default function Profile() {
     ) : (
       <div className="d-grid h-75 gap-3">
         <img src={user.picture} alt={user.name} className="m-auto m-md-5"/>
-        <h2>Username: {user['https://myapp.example.com/username']}</h2>
-        <p>E-mail: {user.email}</p>
-        <p>First name</p>
-        <p>Last name</p>
+        <Form>
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder={user.email} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formUserName">
+        <Form.Label>Username</Form.Label>
+        <Form.Control placeholder={user['https://myapp.example.com/username']} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formFirstName">
+      <Form.Label>First name</Form.Label>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formLastName">
+      <Form.Label>Last name</Form.Label>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Save
+      </Button>
+    </Form>
       </div>
     ))
   );
