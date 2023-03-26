b276084fe92e19892b81a0073c0b4e895c55374c
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-26 01:11:38 -0300

    v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import IssuesTableList from "./components/IssuesTableList";
 
 export default function Home() {
   const { isAuthenticated } = useAuth0();
 
   return isAuthenticated ? (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 gap-3">
-      <h5>Content</h5>
+      <IssuesTableList />
     </div>
   ) : (
-    <div className="d-flex flex-column justify-content-center align-items-center gap-3" style={{height: "75vh"}}>
+    <div
+      className="d-flex flex-column justify-content-center align-items-center gap-3"
+      style={{ height: "75vh" }}
+    >
       <h2>
         This is a <strong>Demo Web Application</strong> named{" "}
         <strong>"Issue Reporting"</strong>
