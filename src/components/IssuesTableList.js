25dcde5ae3023559655828a368222279c463f653
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-26 03:59:47 -0300

    v1

--- src/components/IssuesTableList.js
+++ src/components/IssuesTableList.js
@@ -4,35 +4,38 @@ import data from "../data.json";
 export default function IssuesTableList() {
   return (
     <div className="m-4" style={{ overflowX: "auto" }}>
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Issue No</th>
-          <th>Status</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Priority</th>
-          <th>Assignee</th>
-          <th>Open</th>
-          <th>Close</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data &&
-          data.IssueList.map((issue, index) => 
-            <tr key={index}>
-              <td>{issue.IssueNo}</td>
-              <td>{issue.Status}</td>
-              <td>{issue.Description}</td>
-              <td>{issue.Category}</td>
-              <td>{issue.Priority}</td>
-              <td>{issue.Assignee}</td>
-              <td>{issue.Open}</td>
-              <td>{issue.Close}</td>
-            </tr>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Issue No</th>
+            <th>Status</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Priority</th>
+            <th>Assignee</th>
+            <th>Open</th>
+            <th>Close</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.IssueList ? (
+            data.IssueList.map((issue, index) => (
+              <tr key={index}>
+                <td>{issue.IssueNo}</td>
+                <td>{issue.Status}</td>
+                <td>{issue.Description}</td>
+                <td>{issue.Category}</td>
+                <td>{issue.Priority}</td>
+                <td>{issue.Assignee}</td>
+                <td>{issue.Open}</td>
+                <td>{issue.Close}</td>
+              </tr>
+            ))
+          ) : (
+            <div className="m-5">There aren’t any open issues.</div>
           )}
-      </tbody>
-    </Table>
+        </tbody>
+      </Table>
     </div>
   );
 }
