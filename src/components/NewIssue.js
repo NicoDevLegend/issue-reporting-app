a646934bb75786dd01854c283b29c98aab2661af
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-26 04:42:45 -0300

    v1

--- src/components/NewIssue.js
+++ src/components/NewIssue.js
@@ -2,9 +2,19 @@ import Button from "react-bootstrap/Button";
 
 export default function NewIssue() {
   return (
-    <div>
-      New Issue
-      <Button variant="secondary">+</Button>
+    <div style={{ marginBottom: "-200px;" }}>
+      <stron>New Issue</stron>
+      <Button
+        className="m-1"
+        variant="secondary"
+        size="sm"
+        style={{
+          borderRadius: "inherit",
+          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.75)",
+        }}
+      >
+        +
+      </Button>
     </div>
   );
 }
