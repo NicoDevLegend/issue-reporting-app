54f7eb02af71803e5672529e161da549a39f77ba
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-26 04:47:06 -0300

    v1

--- src/components/NewIssue.js
+++ src/components/NewIssue.js
@@ -2,8 +2,8 @@ import Button from "react-bootstrap/Button";
 
 export default function NewIssue() {
   return (
-    <div style={{ marginBottom: "-200px;" }}>
-      <stron>New Issue</stron>
+    <div style={{ marginBottom: "-200px" }}>
+      <strong>New Issue</strong>
       <Button
         className="m-1"
         variant="secondary"
