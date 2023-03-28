4c4ff7a2bcec94e6c2f0ac00e2aefa5a2c5ae35d
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-28 16:57:06 -0300

    v1

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,12 +1,33 @@
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 export default function Notifications() {
   return (
-    <FontAwesomeIcon
-      icon={faBell}
-      style={{ width: "20px", height: "20px", color: "white" }}
-      className="btn me-3"
-    />
+    <OverlayTrigger
+      trigger="click"
+      placement="auto-start"
+      overlay={
+        <Popover id="notifications">
+          <Popover.Header as="h3">"Notifications"</Popover.Header>
+          <Popover.Body>
+            <strong>An issue was solved</strong>
+            <strong>An issue was solved</strong>
+          </Popover.Body>
+        </Popover>
+      }
+    >
+      <FontAwesomeIcon
+        icon={faBell}
+        style={{
+          width: "20px",
+          height: "20px",
+          color: "white",
+          cursor: "pointer",
+        }}
+        className="me-3 my-auto"
+      />
+    </OverlayTrigger>
   );
 }
