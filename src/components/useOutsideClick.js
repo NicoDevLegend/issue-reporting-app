a5a32cfe5ab29995fb2c8d0f11bf00263670c2c9
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-29 15:10:21 -0300

    v1

--- src/components/useOutsideClick.js
+++ src/components/useOutsideClick.js
@@ -1,21 +1,19 @@
 import { useRef, useEffect } from "react";
 
 export const useOutsideClick = (callback) => {
-  const ref = useRef();
-
-  useEffect(() => {
-    const handleClick = (event) => {
-      if (ref.current && !ref.current.contains(event.target)) {
+    const ref = useRef();
+  
+    useEffect(() => {
+      const handleClick = (event) => {
         callback();
-      }
-    };
-
-    document.addEventListener("click", handleClick);
-
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  }, [ref]);
-
-  return ref;
-};
+      };
+  
+      document.addEventListener('click', handleClick);
+  
+      return () => {
+        document.removeEventListener('click', handleClick);
+      };
+    }, []);
+  
+    return ref;
+  };
