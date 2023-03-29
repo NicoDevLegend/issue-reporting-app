6fb7e5c35d2a553e4d4123656771e88040f4c11c
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-29 14:48:10 -0300

    v1

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useOutsideClick } from "./useOutsideClick";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
-import { Link } from "react-router-dom";
 
 export default function Notifications() {
   const [show, setShow] = useState(false);
+
+  const ref = useOutsideClick(handleClickOutside);
 
   const handleShowNotifications = () => {
     if (show === false) {
@@ -14,24 +16,27 @@ export default function Notifications() {
     }
   };
 
+  const handleNotificationsClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <div className="mx-3 my-auto">
-      <Link to="/notifications">
-      <FontAwesomeIcon
-        icon={faBell}
-        style={{
-          width: "20px",
-          height: "20px",
-          cursor: "pointer",
-          color: "white"
-        }}
-        onClick={handleShowNotifications}
-      />
-      </Link>
+        <FontAwesomeIcon
+          icon={faBell}
+          style={{
+            width: "20px",
+            height: "20px",
+            cursor: "pointer",
+            color: "white",
+          }}
+          onClick={handleShowNotifications}
+        />
       {show && (
         <div
-          className="bg-light position-absolute p-3 rounded"
+          className="bg-light position-absolute p-3 border border-dark-subtle border-2 rounded"
           style={{ right: "5em", width: "150px", height: "auto" }}
+          onClick={handleNotificationsClick}
         >
           <p>Notifications</p>
           <p>Message</p>
