4613f0b50e0ad6999a0fdd40a619f9aba0ecabd3
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-30 12:55:05 -0300

    v1

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -16,7 +16,7 @@ export default function Notifications() {
 
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
-  }, [handleShowNotifications]);
+  }, [handleClickOutside]);
 
   const ref = useRef(null);
 
