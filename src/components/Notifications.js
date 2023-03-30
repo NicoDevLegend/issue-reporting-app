70004cc83d2d4e118607904fb1422fb5e6965a3c
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-03-30 12:11:15 -0300

    v1

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,8 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
-import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Notifications() {
   const [show, setShow] = useState(false);
