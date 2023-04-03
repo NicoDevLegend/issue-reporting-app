37c46a54aa3c3d35269a5cf2ace3e93fd45b2d2f
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-04-03 21:07:45 +0000

    v1

--- src/components/IssuesTableList.js
+++ src/components/IssuesTableList.js
@@ -1,59 +1,105 @@
+import { useState } from "react";
 import Table from "react-bootstrap/Table";
 import data from "../data.json";
 import Dropdown from "react-bootstrap/Dropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTashUndo } from "@fortawesome/free-solid-svg-icons";
 
 export default function IssuesTableList() {
+  const [value, setValue] = useState(null);
+
+  console.log(value);
+
   return (
     <div className="container" style={{ overflow: "auto" }}>
+      <div className="w-100" style={{ height: "40px" }}>
+        {value && (
+          <FontAwesomeIcon
+          icon={faTashUndo}
+          className="position-relative"
+          style={{
+            width: "20px",
+            height: "20px",
+            cursor: "pointer",
+            color: "gray",
+            top: "0", 
+            right: "30%"
+          }}
+            onClick={() => setValue(null)}
+          />
+        )}
+      </div>
       <Table striped bordered hover variant="dark" className="mb-0">
         <thead className="align-middle">
           <tr>
             <th>Issue No</th>
             <th>
-              <Dropdown>
-                <Dropdown.Toggle
-                  variant="dark"
-                >
+              <Dropdown onSelect={(selectedKey) => setValue(selectedKey)}>
+                <Dropdown.Toggle variant="dark">
                   <strong>Status</strong>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item>Not Resolved</Dropdown.Item>
-                  <Dropdown.Item>Resolved</Dropdown.Item>
+                  <Dropdown.Item eventKey="Not Resolved">
+                    Not Resolved
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="Resolved">Resolved</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </th>
             <th>Description</th>
-            <th><Dropdown>
-                <Dropdown.Toggle
-                  variant="dark"
-                >
+            <th>
+              <Dropdown>
+                <Dropdown.Toggle variant="dark">
                   <strong>Category</strong>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item>1</Dropdown.Item>
-                  <Dropdown.Item>2</Dropdown.Item>
-                  <Dropdown.Item>3</Dropdown.Item>
+                  <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">3</Dropdown.Item>
                 </Dropdown.Menu>
-              </Dropdown></th>
-            <th><Dropdown>
-                <Dropdown.Toggle
-                  variant="dark"
-                >
+              </Dropdown>
+            </th>
+            <th>
+              <Dropdown>
+                <Dropdown.Toggle variant="dark">
                   <strong>Priority</strong>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item>Important</Dropdown.Item>
-                  <Dropdown.Item>Very Important</Dropdown.Item>
-                  <Dropdown.Item>Urgent</Dropdown.Item>
+                  <Dropdown.Item eventKey="Important">Important</Dropdown.Item>
+                  <Dropdown.Item eventKey="Very Important">
+                    Very Important
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="Urgent">Urgent</Dropdown.Item>
                 </Dropdown.Menu>
-              </Dropdown></th>
+              </Dropdown>
+            </th>
             <th>Assignee</th>
-            <th>Open</th>
-            <th>Close</th>
+            <th>
+              <Dropdown>
+                <Dropdown.Toggle variant="dark">
+                  <strong>Open</strong>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item eventKey="">Newest</Dropdown.Item>
+                  <Dropdown.Item eventKey="">Oldest</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </th>
+            <th>
+              <Dropdown>
+                <Dropdown.Toggle variant="dark">
+                  <strong>Close</strong>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item eventKey="">Newest</Dropdown.Item>
+                  <Dropdown.Item eventKey="">Oldest</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {data.IssueList ? (
+          {data.IssueList && !value ? (
             data.IssueList.map((issue, index) => (
               <tr key={index}>
                 <td>{issue.IssueNo}</td>
@@ -66,6 +112,21 @@ export default function IssuesTableList() {
                 <td>{issue.Close}</td>
               </tr>
             ))
+          ) : value ? (
+            data.IssueList.filter((issue) => issue.Status === value).map(
+              (issue, index) => (
+                <tr key={index}>
+                  <td>{issue.IssueNo}</td>
+                  <td>{issue.Status}</td>
+                  <td>{issue.Description}</td>
+                  <td>{issue.Category}</td>
+                  <td>{issue.Priority}</td>
+                  <td>{issue.Assignee}</td>
+                  <td>{issue.Open}</td>
+                  <td>{issue.Close}</td>
+                </tr>
+              )
+            )
           ) : (
             <div className="m-5">There aren’t any open issues.</div>
           )}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import NewIssue from "../components/NewIssue";
 import IssuesTableList from "../components/IssuesTableList";
 
-export default function Home() {
+export default function Home({ value }) {
   const { isAuthenticated } = useAuth0();
 
   return isAuthenticated ? (
