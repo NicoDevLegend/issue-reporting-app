3d4f7bf6eb39392adc3bef903e8a231a06fa6eda
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-04-13 13:59:04 +0000

    v1

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,36 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import DATA from "../data.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 export default function SearchForm() {
   const [show, setShow] = useState(false);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(DATA);
+  const [searchResult, setSearchResult] = useState([]);
 
   const handleShowFormControl = () => {
     if (!show) {
       setShow(true);
     } else {
       setShow(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    
+    if (!!search) {
+      const filter = data.IssueList.filter((data) => {
+        return data.Description.toLowerCase().includes(
+          search.toLocaleLowerCase()
+        );
+      });
+      setSearchResult(filter);
+    }else {
+      setSearchResult([]);
     }
   };
 
@@ -23,23 +42,48 @@ export default function SearchForm() {
           placeholder="Search"
           className="d-none d-sm-block me-2"
           aria-label="Search"
+          value={search}
+          onChange={handleChange}
         />
         <Button className="d-none d-sm-block me-5" variant="light">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </Button>
         {show && (
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="d-block d-sm-none position-absolute"
-            style={{
-              width: "90%",
-              Zindex: "20",
-              right: "1em",
-              top: "75px",
-            }}
-            aria-label="Search"
-          />
+          <>
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="d-block d-sm-none position-absolute"
+              style={{
+                width: "90%",
+                Zindex: "20",
+                right: "1em",
+                top: "75px",
+              }}
+              aria-label="Search"
+              value={search}
+              onChange={handleChange}
+            />
+            {search !== "" && (
+              <div
+                className="p-2 position-absolute bg-white text-start rounded-1"
+                style={{
+                  width: "90%",
+                  zIndex: "30",
+                  right: "1em",
+                  top: "115px",
+                  minHeight: "50px",
+                  maxHeight: "300px",
+                  overflowX: "auto",
+                  scrollbarWidth: "thin",
+                }}
+              >
+                {searchResult.map((data, index) => (
+                  <p key={index}>{data.Description}</p>
+                ))}
+              </div>
+            )}
+          </>
         )}
       </Form>
       <Button
