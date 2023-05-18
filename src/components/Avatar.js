fb5462ce82c43f5c1b067aea2a051264cc17228f
NicoDev <121879206+NicoDevLegend@users.noreply.github.com>
2023-05-18 14:48:02 -0300

    v1

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,6 @@
 import { Nav, NavItem, NavLink, Dropdown } from "react-bootstrap";
 import LogOutButton from "./LogOutButton";
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { useUserDataContext } from "./UserDataProvider";
